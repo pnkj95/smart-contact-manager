e7fadc41e35f228b54933b3b50218383dc7de520
Pankaj Pathak <47478416+pnkj95@users.noreply.github.com>
2023-12-27 23:06:17 +0530

    Update SmartContactManagerApplication.java
    Changes required to run the app on external tomcat

--- src/main/java/com/smart/SmartContactManagerApplication.java
+++ src/main/java/com/smart/SmartContactManagerApplication.java
@@ -2,13 +2,20 @@ package com.smart;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SmartContactManagerApplication {
+public class SmartContactManagerApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(SmartContactManagerApplication.class, args);
 		System.out.println("Congratulations! Your application has been started successfully...");
 	}
+
+	@Override
+    	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        	return builder.sources(SmartContactManagerApplication.class);
+    	}
 
 }
